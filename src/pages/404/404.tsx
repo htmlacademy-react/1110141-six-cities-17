1fbe70b8323e2547d65379230028deddb0914f20
Kolyada Danil <kd_kein@mail.ru>
2024-11-27 21:34:13 +1000

    3.19. Маршрут по тайным тропам: Исправлен путь до gif

--- src/pages/404/404.tsx
+++ src/pages/404/404.tsx
@@ -8,7 +8,7 @@ function Page404(): JSX.Element {
           <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: '20px', paddingTop: '100px' }} className="page__main-flex-box flex-box">
             <h1 style={{ textAlign: 'center', margin: '0', fontSize: '100px' }}>404</h1>
             <h3 style={{ textAlign: 'center', margin: '0' }}>Страницу мы не нашли, зато у нас есть танцующий помидор!</h3>
-            <img style={{ margin: '0 auto', display: 'block' }} src="../../public/img/tomato.gif" alt="Page not found :(" />
+            <img style={{ margin: '0 auto', display: 'block' }} src="../../img/tomato.gif" alt="Page not found :(" />
             <Link style={{ textAlign: 'center', margin: '0', fontSize: '20px' }} to="/">Вернуться на главную</Link>
           </div>
         </div>
