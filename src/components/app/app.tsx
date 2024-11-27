10005a9926d21000c833baec1658eaa9e2d06637
Kolyada Danil <kd_kein@mail.ru>
2024-11-27 21:19:41 +1000

    3.19. Маршрут по тайным тропам: Добавлена маршрутизация по страницам

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,9 @@
 import Main from '../../pages/main/main';
+import Page404 from '../../pages/404/404';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Favorites from '../../pages/favorites/favorites';
+import Login from '../../pages/login/login';
+import Offer from '../../pages/offer/offer';
 
 type AppProps = {
   foundPlacesCount: number;
@@ -6,7 +11,18 @@ type AppProps = {
 
 function App({ foundPlacesCount }: AppProps): JSX.Element {
   return (
-    <Main foundPlacesCount={foundPlacesCount} />
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Main foundPlacesCount={foundPlacesCount} />} />
+          <Route path="favorites" element={<Favorites />} />
+          <Route path="login" element={<Login />} />
+          <Route path="offer/:id" element={<Offer />} />
+        </Route>
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
