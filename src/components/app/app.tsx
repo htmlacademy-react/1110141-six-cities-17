114352a3d887fbee9eca91e45946f4a3383b3785
Kolyada Danil <kd_kein@mail.ru>
2024-11-27 22:17:58 +1000

    3.19. Маршрут по тайным тропам: Добавлен приватый маршурт

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Favorites from '../../pages/favorites/favorites';
 import Login from '../../pages/login/login';
 import Offer from '../../pages/offer/offer';
+import PrivateRoute from '../private-route/private-route';
+import { AuthorizationStatus, AppRoute } from '../../const';
 
 type AppProps = {
   foundPlacesCount: number;
@@ -14,11 +16,17 @@ function App({ foundPlacesCount }: AppProps): JSX.Element {
 
     <BrowserRouter>
       <Routes>
-        <Route path="/">
+        <Route path={AppRoute.Main}>
           <Route index element={<Main foundPlacesCount={foundPlacesCount} />} />
-          <Route path="favorites" element={<Favorites />} />
-          <Route path="login" element={<Login />} />
-          <Route path="offer/:id" element={<Offer />} />
+          <Route path={AppRoute.Favorites}
+            element={
+              <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+                <Favorites />
+              </PrivateRoute>
+            }
+          />
+          <Route path={AppRoute.Login} element={<Login />} />
+          <Route path={AppRoute.Offer} element={<Offer />} />
         </Route>
         <Route path="*" element={<Page404 />} />
       </Routes>
