17dd4e827781452024cedfc53c65962f0d713308
Kolyada Danil <kd_kein@mail.ru>
2024-12-01 22:39:29 +1000

    4.21. Обратная сторона реальности: Описаны типы предложений жилья

---
+++ src/types/offers.ts
@@ -0,0 +1,46 @@
+import { HousingType } from '../const';
+
+type Location = {
+  latitude: number;
+  longitude: number;
+  zoom: number;
+}
+
+type City = {
+  name: string;
+  location: Location;
+}
+
+type Host = {
+  name: string;
+  avatarUrl: string;
+  isPro: boolean;
+}
+
+export type CompactOffer = {
+  id: string;
+  title: string;
+  type: HousingType;
+  price: number;
+  city: City;
+  location: Location;
+  isFavorite: boolean;
+  isPremium: boolean;
+  rating: number;
+  previewImage: string;
+}
+
+export type DetailedOffer = {
+  offer: CompactOffer;
+  description: string;
+  bedrooms: number;
+  goods: string[];
+  host: Host;
+  images: string[];
+  maxAdults: number;
+}
+
+export type Offer = CompactOffer | DetailedOffer;
+
+export type Offers = Offer[];
+
