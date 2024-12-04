fb77c2efe0777ff9de8654cbd0010ed1e2a1bb40
Kolyada Danil <kd_kein@mail.ru>
2024-12-04 21:53:48 +1000

    4.21. Обратная сторона реальности: Компонент карточки предложения был научен принимать данные через пропсы

--- src/components/place-card/place-card.tsx
+++ src/components/place-card/place-card.tsx
@@ -1,14 +1,57 @@
-function PlaceCard(): JSX.Element {
+import { CompactOffer } from '../../types/offers';
+
+import { AuthorizationStatus } from '../../const';
+
+import { Navigate } from 'react-router-dom';
+import { AppRoute } from '../../const';
+import { useState } from 'react';
+
+/** Величина на которую нужно умножить рейтинг чтобы получить проценты */
+const MULTIPLYER_FOR_PERCANTAGE_CONVERSION = 20;
+
+type PlaceCardProps = {
+  offer: CompactOffer;
+}
+
+function convertRatingToStars(rating: number) {
+  if (rating) {
+    return MULTIPLYER_FOR_PERCANTAGE_CONVERSION * Math.round(rating);
+  }
+  return 0;
+}
+
+function PlaceCard({ offer }: PlaceCardProps): JSX.Element {
+
+  const { title, price, type, previewImage, isPremium, rating } = offer;
+  const ratingStars = convertRatingToStars(rating).toString(10);
+
+  const [redirect, setRedirect] = useState(false);
+
+  function handleBookmarkClick(authorizationStatus: string) {
+    if (!Object.values(AuthorizationStatus).includes(authorizationStatus as AuthorizationStatus)) {
+      return null;
+    }
+    if ((authorizationStatus as AuthorizationStatus) !== AuthorizationStatus.Auth) {
+      setRedirect(true);
+    }
+  }
+
+  if (redirect) {
+    return <Navigate to={AppRoute.Login} />;
+  }
+
   return (
     <article className="cities__card place-card">
-      <div className="place-card__mark">
-        <span>Premium</span>
-      </div>
+      {isPremium && (
+        <div className="place-card__mark">
+          <span>Premium</span>
+        </div>
+      )}
       <div className="cities__image-wrapper place-card__image-wrapper">
         <a href="#">
           <img
             className="place-card__image"
-            src="img/apartment-01.jpg"
+            src={previewImage}
             width={260}
             height={200}
             alt="Place image"
@@ -18,12 +61,13 @@ function PlaceCard(): JSX.Element {
       <div className="place-card__info">
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
-            <b className="place-card__price-value">€120</b>
+            <b className="place-card__price-value">€{price}</b>
             <span className="place-card__price-text">/&nbsp;night</span>
           </div>
           <button
             className="place-card__bookmark-button button"
             type="button"
+            onClick={() => handleBookmarkClick(AuthorizationStatus.NoAuth)}
           >
             <svg
               className="place-card__bookmark-icon"
@@ -37,16 +81,16 @@ function PlaceCard(): JSX.Element {
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
-            <span style={{ width: '80%' }} />
+            <span style={{ width: `${ratingStars}%` }} />
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
         <h2 className="place-card__name">
           <a href="#">
-            Beautiful &amp; luxurious apartment at great location
+            {title}
           </a>
         </h2>
-        <p className="place-card__type">Apartment</p>
+        <p className="place-card__type">{type}</p>
       </div>
     </article>
   );
