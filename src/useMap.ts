22e722e56da312e3228fdd28030a4b40b116e137
Kolyada Danil <kd_kein@mail.ru>
2024-12-10 23:06:19 +1000

    5.18. Больше подробностей (часть 1): Добавлен кастомный хук useMap

---
+++ src/useMap.ts
@@ -0,0 +1,47 @@
+import { MutableRefObject, useEffect, useRef, useState } from 'react';
+
+import leaflet from 'leaflet';
+
+import { City } from './types/cities';
+
+type UseMapProps = {
+  mapRef: MutableRefObject<HTMLSelectElement | null>;
+  city: City;
+}
+
+function useMap({ mapRef, city }: UseMapProps) {
+  const [map, setMap] = useState<leaflet.Map | null>(null);
+  const isRenderedRef = useRef(false);
+
+  useEffect(() => {
+    if (!isRenderedRef.current && mapRef.current !== null) {
+      const mapInstance = leaflet.map(mapRef?.current, {
+        center: [
+          city.latitude,
+          city.longitude,
+        ],
+        zoom: city.zoom,
+      });
+
+      leaflet
+        .tileLayer(
+          'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+          {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+          },
+        )
+        .addTo(mapInstance);
+
+      setMap(mapInstance);
+      isRenderedRef.current = true;
+    }
+
+    return () => {
+      isRenderedRef.current = false;
+    };
+  }, [mapRef, city]);
+
+  return map;
+}
+
+export { useMap };
