248d3da5b3494c46179088ffd99d4e2e9990826f
Kolyada Danil <kd_kein@mail.ru>
2024-12-11 22:24:07 +1000

    5.18. Больше подробностей (часть 1): Стейт активной карточки предложения перенесён в main.tsx для "общения" между карточками в списке и иконками карты

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -6,49 +6,71 @@ import 'leaflet/dist/leaflet.css';
 import { useMap } from '../../useMap';
 
 import { City } from '../../types/cities';
-import {
-  URL_MARKER_DEFAULT,
-  // URL_MARKER_CURRENT
-} from '../../const';
+import { URL_MARKER_DEFAULT, URL_MARKER_CURRENT } from '../../const';
 import { CompactOffer } from '../../types/offers';
 
 
 type MapProps = {
   city: City;
   offers: CompactOffer[];
+  cardActive: string | null;
 }
 
-function Map({ city, offers }: MapProps): JSX.Element {
+function Map({ city, offers, cardActive }: MapProps): JSX.Element {
 
   const mapRef = useRef(null);
   const map = useMap({ mapRef, city });
 
-  const defaultCustomIcon = leaflet.icon({
-    iconUrl: URL_MARKER_DEFAULT,
-    iconSize: [40, 40],
-    iconAnchor: [20, 40],
-  });
+  /** Добавит иконки в неизменяемый контейнер */
+  const defaultIconRef = useRef<leaflet.Icon | null>(null);
+  const currentIconRef = useRef<leaflet.Icon | null>(null);
 
-  // const currentCustomIcon = leaflet.icon({
-  //   iconUrl: URL_MARKER_CURRENT,
-  //   iconSize: [40, 40],
-  //   iconAnchor: [20, 40],
-  // });
+  /** Проверит созданы ли уже иконки, если нет — создаст */
+  if (!defaultIconRef.current || !currentIconRef.current) {
+    defaultIconRef.current = leaflet.icon({
+      iconUrl: URL_MARKER_DEFAULT,
+      iconSize: [40, 40],
+      iconAnchor: [20, 40],
+    });
+
+    currentIconRef.current = leaflet.icon({
+      iconUrl: URL_MARKER_CURRENT,
+      iconSize: [40, 40],
+      iconAnchor: [20, 40],
+    });
+  }
+
+  /** Создаст слой для карты */
+  const layer = useRef<leaflet.LayerGroup | null>(null);
 
   useEffect(() => {
     if (map) {
+
+      layer.current = leaflet.layerGroup().addTo(map);
+
       offers.forEach((offer) => {
+
+        if (!currentIconRef.current || !defaultIconRef.current) {
+          return;
+        }
+
+        const iconToSet: leaflet.Icon = cardActive === offer.id ? currentIconRef.current : defaultIconRef.current;
+
         leaflet
           .marker({
             lat: offer.location.latitude,
             lng: offer.location.longitude,
           }, {
-            icon: defaultCustomIcon,
+            icon: iconToSet,
           })
-          .addTo(map);
+          .addTo(layer.current ? layer.current : map);
+      });
+
+      return (() => {
+        layer.current?.clearLayers();
       });
     }
-  }, [map, offers, defaultCustomIcon]);
+  }, [map, offers, cardActive]);
 
   return (
     <section
