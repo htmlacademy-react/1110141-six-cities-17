5786878312a9ec652401c32db2d4532a0ee92433
Kolyada Danil <kd_kein@mail.ru>
2024-12-22 17:55:55 +1000

    6.15. Контроль и ограничения (часть 1): Добавлен компонент LocationsList

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,6 +1,7 @@
 import Logo from '../../components/logo/logo';
 import PlaceList from '../../components/places-list/place-list';
 import Map from '../../components/map/map';
+import LocationsList from '../../components/locations-list/locations-list';
 
 import { Helmet } from 'react-helmet-async';
 import { useState } from 'react';
@@ -64,38 +65,7 @@ function Main({ foundPlacesCount, offers }: MainPageProps): JSX.Element {
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
           <section className="locations container">
-            <ul className="locations__list tabs__list">
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Paris</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Cologne</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Brussels</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item tabs__item--active">
-                  <span>Amsterdam</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Hamburg</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Dusseldorf</span>
-                </a>
-              </li>
-            </ul>
+            <LocationsList />
           </section>
         </div>
         <div className="cities">
