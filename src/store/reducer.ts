165170c8fab979aa855b68bafb66136991c7d1e9
Kolyada Danil <kd_kein@mail.ru>
2024-12-22 18:00:03 +1000

    6.15. Контроль и ограничения (часть 1): Переписан reducer: город в initialState установлен Paris, предложения в initialState установлены как предложения города Paris. Action changeOffersByCity теперь не принимает полезную нагрузку, а берёт город для сортировки предложений напрямую из store

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -4,19 +4,18 @@ import { Cities } from '../const';
 import { changeCity, changeOffersByCity } from './actions';
 
 const initialState = {
-  city: Cities.Amsterdam,
-  offers: offers,
+  city: Cities.Paris,
+  offers: offers.filter((offer) => offer.city.name as Cities === Cities.Paris),
 };
 
 export const reducer = createReducer(initialState, (builder) => {
   builder.addCase(changeCity, (state, action) => {
-    if (action.payload && Object.values(Cities).includes(action.payload)) {
-      state.city = action.payload;
+    const payload: Cities = action.payload;
+    if (action.payload && Object.values(Cities).includes(payload)) {
+      state.city = payload;
     }
   })
-    .addCase(changeOffersByCity, (state, action) => {
-      if (action.payload) {
-        state.offers = action.payload;
-      }
+    .addCase(changeOffersByCity, (state) => {
+      state.offers = offers.filter((offer) => offer.city.name as Cities === state.city);
     });
 });
