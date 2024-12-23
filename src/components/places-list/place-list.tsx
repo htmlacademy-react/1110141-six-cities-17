023f78959f65d3ccb4f33f0a0dbc434836d95911
Kolyada Danil <kd_kein@mail.ru>
2024-12-23 12:02:24 +1000

    6.16. Контроль и ограничения (часть 2): Компонент PlaceList обучен сортировать предложения исходя из глобального состояния (store)

--- src/components/places-list/place-list.tsx
+++ src/components/places-list/place-list.tsx
@@ -1,6 +1,8 @@
 import PlaceCard from '../place-card/place-card';
 
-import { CompactOffers } from '../../types/offers';
+import { CompactOffer, CompactOffers } from '../../types/offers';
+import { useAppSelector } from '../../hooks';
+import { SortTypes } from '../../const';
 
 type PlaceListProps = {
   offers: CompactOffers;
@@ -8,11 +10,57 @@ type PlaceListProps = {
   handleMouseout: () => void;
 }
 
+function sortPriceToHight(firstOffer: CompactOffer, secondOffer: CompactOffer) {
+  if (firstOffer.price > secondOffer.price) {
+    return 1;
+  } else if (firstOffer.price < secondOffer.price) {
+    return -1;
+  }
+  return 0;
+}
+
+function sortPriceToLow(firstOffer: CompactOffer, secondOffer: CompactOffer) {
+  if (firstOffer.price < secondOffer.price) {
+    return 1;
+  } else if (firstOffer.price > secondOffer.price) {
+    return -1;
+  }
+  return 0;
+}
+
+function sortTop(firstOffer: CompactOffer, secondOffer: CompactOffer) {
+  if (firstOffer.rating < secondOffer.rating) {
+    return 1;
+  } else if (firstOffer.rating > secondOffer.rating) {
+    return -1;
+  }
+  return 0;
+}
+
 function PlaceList({ offers, handleMouseOver, handleMouseout }: PlaceListProps) {
+
+  const sort = useAppSelector((state) => state.sort);
+  const currentSort = sort.find((item) => item.isActive === true);
+
+  let sortedOffers: CompactOffers = structuredClone(offers);
+
+  switch (currentSort?.title) {
+    case SortTypes.PriceToHigh:
+      sortedOffers.sort(sortPriceToHight);
+      break;
+    case SortTypes.PriceToLow:
+      sortedOffers.sort(sortPriceToLow);
+      break;
+    case SortTypes.Top:
+      sortedOffers.sort(sortTop);
+      break;
+    default:
+      sortedOffers = offers;
+  }
 
   return (
     <div className="cities__places-list places__list tabs__content">
-      {offers.map((offer) => (
+      {sortedOffers.map((offer) => (
         <PlaceCard
           key={offer.id}
           offer={offer}
