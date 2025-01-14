9a69603cfbcbad3a8ce87ecb64506c69015d7ef4
Danil Kolyada <kd_kein@mail.ru>
2025-01-14 20:44:15 +0300

    7.14. Истина где-то на сервере: К событиям добавлены пространства имён

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -2,9 +2,9 @@ import { createAction } from '@reduxjs/toolkit';
 import { AuthorizationStatus, Cities, SortTypes } from '../const';
 import { CompactOffers } from '../types/offers';
 
-export const changeCity = createAction<Cities>('changeCity');
-export const changeOffersByCity = createAction('changeOffersByCity');
-export const changeActiveSort = createAction<SortTypes>('changeActiveSort');
-export const loadOffers = createAction<CompactOffers>('loadOffers');
-export const requireAuthorization = createAction<AuthorizationStatus>('requireAuthorization');
+export const changeCity = createAction<Cities>('app/changeCity');
+export const changeOffersByCity = createAction('app/changeOffersByCity');
+export const changeActiveSort = createAction<SortTypes>('app/changeActiveSort');
+export const loadOffers = createAction<CompactOffers>('data/loadOffers');
+export const requireAuthorization = createAction<AuthorizationStatus>('user/requireAuthorization');
 
