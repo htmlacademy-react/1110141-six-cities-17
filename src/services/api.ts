bef0caccfa49aa037186d722dbfa6db99789b066
Danil Kolyada <kd_kein@mail.ru>
2025-01-15 22:24:13 +0300

    7.14. Истина где-то на сервере: Добавлены пояснительные комментарии

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,14 +5,17 @@ import { getToken } from './token';
 const BACKEND_URL = 'https://16.design.htmlacademy.pro/six-cities';
 const REQUEST_TIMEOUT = 5000;
 
+/** Создаёт экземпляр axios с предустановленными параметрами */
 export const createAPI = (): AxiosInstance => {
   const api = axios.create({
     baseURL: BACKEND_URL,
     timeout: REQUEST_TIMEOUT,
   });
 
+  /** Добавляет интерсептор который вмешивается в каждый запрос */
   api.interceptors.request.use(
     (config: InternalAxiosRequestConfig) => {
+      /** Получает токен и добавляет его в заголовок x-token */
       const token = getToken();
 
       if (token && config.headers) {
