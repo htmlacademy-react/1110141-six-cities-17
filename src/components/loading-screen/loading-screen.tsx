f9174253abea6906e4a1f858758f9c99f7d65393
Danil Kolyada <kd_kein@mail.ru>
2025-01-19 21:42:06 +0300

    7.14. Истина где-то на сервере: Добавлен компонент загрузки

--- src/components/loading-screen/loading-screen.tsx
+++ src/components/loading-screen/loading-screen.tsx
@@ -1,6 +1,19 @@
 function LoadingScreen(): JSX.Element {
   return (
-    <p>Loading...</p>
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: '100vw',
+      height: '100vh',
+      background: 'white',
+      position: 'fixed',
+      top: 0,
+      left: 0,
+    }}
+    >
+      <img src="../../../../img/spinner.gif" alt="" />
+    </div>
   );
 }
 
