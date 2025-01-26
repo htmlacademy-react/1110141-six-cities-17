718d5001801d0d6b09789f39752eaf150a45270c
Danil Kolyada <kd_kein@mail.ru>
2025-01-26 19:10:40 +0300

    7.15. Добро пожаловать, или посторонним вход воспрещён (часть 2): Добавлена загрузка предложения на странице предложения с сервера. Загружаются также соседние предложения и комментарии

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -26,22 +26,30 @@ function Header({ isShortHeader }: HeaderProps): JSX.Element {
                 <ul className="header__nav-list">
                   {
                     isUserAuthorized ? (
-                      <li className="header__nav-item user">
-                        <a
-                          className="header__nav-link header__nav-link--profile"
-                          href="#"
-                        >
-                          <div
-                            className="header__avatar-wrapper user__avatar-wrapper"
-                            style={{ backgroundImage: `url(${userData?.avatarUrl})` }}
+                      <>
+                        <li className="header__nav-item user">
+                          <a
+                            className="header__nav-link header__nav-link--profile"
+                            href="#"
                           >
-                          </div>
-                          <span className="header__user-name user__name">
-                            {userData?.email}
-                          </span>
-                          <span className="header__favorite-count">3</span>
-                        </a>
-                      </li>
+                            <div
+                              className="header__avatar-wrapper user__avatar-wrapper"
+                              style={{ backgroundImage: `url(${userData?.avatarUrl})` }}
+                            >
+                            </div>
+                            <span className="header__user-name user__name">
+                              {userData?.email}
+                            </span>
+                            <span className="header__favorite-count">3</span>
+                          </a>
+                        </li>
+                        {/* TODO: допилить логаут
+                        <li className="header__nav-item">
+                          <a className="header__nav-link" href="#">
+                            <span className="header__signout">Sign out</span>
+                          </a>
+                        </li> */}
+                      </>
                     ) : (
                       <li className="header__nav-item user">
                         <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Login}>
--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,12 +1,15 @@
 import { createAction } from '@reduxjs/toolkit';
 import { AuthorizationStatus, Cities, SortTypes } from '../const';
-import { CompactOffers } from '../types/offers';
+import { CompactOffers, DetailedOffer } from '../types/offers';
 import { Comments } from '../types/comments';
 import { UserData } from '../types/userData';
 
 export const changeCity = createAction<Cities>('app/changeCity');
 export const changeActiveSort = createAction<SortTypes>('app/changeActiveSort');
 export const loadOffers = createAction<CompactOffers>('data/loadOffers');
+export const loadDetailedOffer = createAction<DetailedOffer>('data/loadDetailedOffer');
+export const loadNearbyOffers = createAction<CompactOffers>('data/loadNearbyOffers');
+export const loadOfferComments = createAction<Comments>('data/loadOfferComments');
 export const requireAuthorization = createAction<AuthorizationStatus>('user/requireAuthorization');
 export const loadComments = createAction<Comments>('comments/loadComments');
 export const setError = createAction<string | null>('app/setError');
