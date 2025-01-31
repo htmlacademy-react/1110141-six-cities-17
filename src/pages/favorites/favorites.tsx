ca491824bbb9242369813e460914e27524baa4fc
Kolyada Danil <kd_kein@mail.ru>
2025-01-31 22:18:32 +0300

    8.13. Оптимизируй это (часть 2): Компоненты FavoritesLocation и FavoritesOffer внедрены в страницу favorites

--- src/pages/favorites/favorites.tsx
+++ src/pages/favorites/favorites.tsx
@@ -1,11 +1,15 @@
 import { Helmet } from 'react-helmet-async';
-import { Link } from 'react-router-dom';
 
 import { CompactOffers } from '../../types/offers';
 
-import { convertRatingToStars, getOfferLink } from '../../utils';
 import { useAppSelector } from '../../hooks';
+
 import Header from '../../components/header/header';
+import FavoritesLocations from '../../components/favorites-locations/favorites-location';
+
+import { useMemo } from 'react';
+
+import { Cities } from '../../const';
 
 type SortedOffers = {
   [key: string]: CompactOffers;
@@ -15,17 +19,19 @@ function Favorites(): JSX.Element {
 
   const offers = useAppSelector((state) => state.offers);
 
-  const favoriteOffers = offers.filter((offer) => offer.isFavorite);
-  const sortedOffers: SortedOffers = {};
+  const favoriteOffers = useMemo(() => (
+    offers.filter((offer) => offer.isFavorite)
+  ), [offers]);
 
-  favoriteOffers.forEach((offer) => {
-
-    if (!sortedOffers[offer.city.name]) {
-      sortedOffers[offer.city.name] = []; // Инициализируем пустой массив, если его нет
-    }
-
-    sortedOffers[offer.city.name].push(offer);
-  });
+  const sortedOffers = useMemo(() =>
+    favoriteOffers.reduce((accumulator, offer) => {
+      const cityName = offer.city.name;
+      if (!accumulator[cityName]) {
+        accumulator[cityName] = [];
+      }
+      accumulator[cityName].push(offer);
+      return accumulator;
+    }, {} as SortedOffers), [favoriteOffers]);
 
   return (
     <div className="page">
@@ -38,75 +44,13 @@ function Favorites(): JSX.Element {
           <section className="favorites">
             <h1 className="favorites__title">Saved listing</h1>
             <ul className="favorites__list">
-              {Object.keys(sortedOffers).map((city) => (
-                <li key={city} className="favorites__locations-items">
-                  <div className="favorites__locations locations locations--current">
-                    <div className="locations__item">
-                      <a className="locations__item-link" href="#">
-                        <span>{city}</span>
-                      </a>
-                    </div>
-                  </div>
-                  <div className="favorites__places">
-                    {Object.values(sortedOffers[city]).map((offer) => (
-                      <article key={offer.id} className="favorites__card place-card">
-                        {offer.isPremium && (
-                          <div className="place-card__mark">
-                            <span>Premium</span>
-                          </div>
-                        )}
-                        <div className="favorites__image-wrapper place-card__image-wrapper">
-                          <Link to={getOfferLink(offer.id)}>
-                            <img
-                              className="place-card__image"
-                              src={offer.previewImage}
-                              width={150}
-                              height={110}
-                              alt="Place image"
-                            />
-                          </Link>
-                        </div>
-                        <div className="favorites__card-info place-card__info">
-                          <div className="place-card__price-wrapper">
-                            <div className="place-card__price">
-                              <b className="place-card__price-value">€{offer.price}</b>
-                              <span className="place-card__price-text">
-                                /&nbsp;night
-                              </span>
-                            </div>
-                            <button
-                              className="place-card__bookmark-button place-card__bookmark-button--active button"
-                              type="button"
-                            >
-                              <svg
-                                className="place-card__bookmark-icon"
-                                width={18}
-                                height={19}
-                              >
-                                <use xlinkHref="#icon-bookmark" />
-                              </svg>
-                              <span className="visually-hidden">In bookmarks</span>
-                            </button>
-                          </div>
-                          <div className="place-card__rating rating">
-                            <div className="place-card__stars rating__stars">
-                              <span style={{ width: `${convertRatingToStars(offer.rating)}%` }} />
-                              <span className="visually-hidden">Rating</span>
-                            </div>
-                          </div>
-                          <h2 className="place-card__name">
-                            <Link to={getOfferLink(offer.id)}>
-                              {offer.title}
-                            </Link>
-                          </h2>
-                          <p className="place-card__type">{offer.type}</p>
-                        </div>
-                      </article>
-                    ))}
-                  </div>
-                </li>
-              )
-              )}
+              {(Object.keys(sortedOffers) as Cities[]).map((city) => (
+                <FavoritesLocations
+                  key={city}
+                  city={city}
+                  offers={sortedOffers[city]}
+                />
+              ))}
             </ul>
           </section>
         </div>
