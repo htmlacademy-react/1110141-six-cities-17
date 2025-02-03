df7f58432dbe885e0b7d0a11553020393aaf02f1
Kolyada Danil <kd_kein@mail.ru>
2025-02-03 16:32:29 +0300

    9.37. Подготовиться к защите проекта): Добавлен вывод соотвествующего экрана при отсутствии favorites элементов

--- src/pages/favorites/favorites.tsx
+++ src/pages/favorites/favorites.tsx
@@ -41,41 +41,76 @@ function Favorites(): JSX.Element {
       return accumulator;
     }, {} as SortedOffers), [favoriteOffers]);
 
-  return (
-    <div className="page">
-      <Helmet>
-        <title>6 cities: favorites</title>
-      </Helmet>
-      <Header />
-      <main className="page__main page__main--favorites">
-        <div className="page__favorites-container container">
-          <section className="favorites">
-            <h1 className="favorites__title">Saved listing</h1>
-            <ul className="favorites__list">
-              {(Object.keys(sortedOffers) as Cities[]).map((city) => (
-                <FavoritesLocations
-                  key={city}
-                  city={city}
-                  offers={sortedOffers[city]}
-                />
-              ))}
-            </ul>
-          </section>
-        </div>
-      </main>
-      <footer className="footer container">
-        <a className="footer__logo-link" href="main.html">
-          <img
-            className="footer__logo"
-            src="img/logo.svg"
-            alt="6 cities logo"
-            width={64}
-            height={33}
-          />
-        </a>
-      </footer>
-    </div>
-  );
+  if (Object.keys(sortedOffers).length) {
+    return (
+      <div className="page">
+        <Helmet>
+          <title>6 cities: favorites</title>
+        </Helmet>
+        <Header />
+        <main className="page__main page__main--favorites">
+          <div className="page__favorites-container container">
+            <section className="favorites">
+              <h1 className="favorites__title">Saved listing</h1>
+              <ul className="favorites__list">
+                {(Object.keys(sortedOffers) as Cities[]).map((city) => (
+                  <FavoritesLocations
+                    key={city}
+                    city={city}
+                    offers={sortedOffers[city]}
+                  />
+                ))}
+              </ul>
+            </section>
+          </div>
+        </main>
+        <footer className="footer container">
+          <a className="footer__logo-link" href="main.html">
+            <img
+              className="footer__logo"
+              src="img/logo.svg"
+              alt="6 cities logo"
+              width={64}
+              height={33}
+            />
+          </a>
+        </footer>
+      </div>
+    );
+  } else {
+    return (
+      <div className="page page--favorites-empty">
+        <Helmet>
+          <title>6 cities: favorites</title>
+        </Helmet>
+        <Header />
+        <main className="page__main page__main--favorites page__main--favorites-empty">
+          <div className="page__favorites-container container">
+            <section className="favorites favorites--empty">
+              <h1 className="visually-hidden">Favorites (empty)</h1>
+              <div className="favorites__status-wrapper">
+                <b className="favorites__status">Nothing yet saved.</b>
+                <p className="favorites__status-description">
+                  Save properties to narrow down search or plan your future trips.
+                </p>
+              </div>
+            </section>
+          </div>
+        </main>
+        <footer className="footer container">
+          <a className="footer__logo-link" href="main.html">
+            <img
+              className="footer__logo"
+              src="img/logo.svg"
+              alt="6 cities logo"
+              width={64}
+              height={33}
+            />
+          </a>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default Favorites;
